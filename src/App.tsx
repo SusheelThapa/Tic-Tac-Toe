245c24bf32345d88a5c21a31cb2c85235193e5dd
SusheelThapa <susheelthapa90@gmail.com>
2024-05-31 07:05:53 +0545

    Add sound to make it interactive

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
+import useSound from "use-sound";
+
 import Board from "./components/Board";
 import Header from "./components/Header";
 import Score from "./components/Score";
 
+import noteHigh from "./assets/audio/note-high.mp3";
+import noteLow from "./assets/audio/note-low.mp3";
+
 const App = () => {
   const [mute, setMute] = useState<boolean>(false);
-  const [board, setBoard] = useState(Array(9).fill(''));
-  const [isXTurn, setIsXTurn] = useState(true); 
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  const [playHighNote] = useSound(noteHigh, { volume: mute ? 0 : 1 });
+  const [playLowNote] = useSound(noteLow, { volume: mute ? 0 : 1 });
 
   const handleCellClick = (index: number) => {
     const newBoard = [...board];
     // Check if the cell is already filled
-    if (newBoard[index] === '') {
-      newBoard[index] = isXTurn ? 'X' : 'O';
+    if (newBoard[index] === "") {
+      newBoard[index] = isXTurn ? "X" : "O";
       setBoard(newBoard);
-      setIsXTurn(!isXTurn);  // Toggle turn
+
+      if (isXTurn) {
+        playHighNote();
+      } else {
+        playLowNote();
+      }
+
+      setIsXTurn(!isXTurn); // Toggle turn
     }
   };
 
