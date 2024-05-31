3dc3a6e75ee4257dd8c0aed875cc80a651b959bd
SusheelThapa <susheelthapa90@gmail.com>
2024-05-31 07:27:32 +0545

    Create Score section

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,6 +1,20 @@
 const Score = () => {
-    return <div>This is score</div>;
-  };
-  
-  export default Score;
-  
+  return (
+    <div className="flex justify-center items-center gap-20 text-lg tracking-wider">
+      <div className="text-center">
+        <div>PLAYER(X)</div>
+        <div className="text-5xl">0</div>
+      </div>
+      <div className="text-center">
+        <div>TIE</div>
+        <div className="text-5xl">0</div>
+      </div>
+      <div className="text-center">
+        <div>COMPUTER(O)</div>
+        <div className="text-5xl">2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Score;
