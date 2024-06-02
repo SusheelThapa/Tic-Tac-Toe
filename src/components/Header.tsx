e17582dcaab4c0a3f3eb597000350971609d1666
SusheelThapa <susheelthapa90@gmail.com>
2024-06-02 22:06:14 +0545

    feat: share button is added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from "react-icons/fa6";
+import { FaGithub, FaShareNodes } from "react-icons/fa6";
 import { BiSolidUpvote } from "react-icons/bi";
 import logo from "../assets/images/logo.png";
 import { GoMute } from "react-icons/go";
@@ -6,19 +6,54 @@ import { GoUnmute } from "react-icons/go";
 import { Tour } from "shepherd.js/tour";
 import { Link } from "react-router-dom";
 import { FaPlusCircle } from "react-icons/fa";
+import Modal from "./Modal";
+import { useState } from "react";
 
 interface Props {
   mute: boolean;
   handleMuteButton: (value: boolean) => void;
   how_to_play: Tour;
-  start_tour:Tour
+  start_tour: Tour;
 }
 
-const Header = ({ mute, handleMuteButton, how_to_play,start_tour }: Props) => {
+const Header = ({ mute, handleMuteButton, how_to_play, start_tour }: Props) => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const handleShare = async () => {
+    const shareData = {
+      title: "Tic Tac Toe",
+      text: "Check out this awesome website!",
+      url: window.location.href,
+    };
+
+    if (navigator.share) {
+      try {
+        await navigator.share(shareData);
+        console.log("Content shared successfully");
+      } catch (error) {
+        console.error("Error sharing content:", error);
+      }
+    } else {
+      // Fallback for browsers that do not support the Web Share API
+      try {
+        await navigator.clipboard.writeText(window.location.href);
+        setModalMessage(
+          "Link copied to clipboard. You can now share it manually."
+        );
+        setShowModal(true);
+      } catch (error) {
+        console.error("Failed to copy the link:", error);
+      }
+    }
+  };
+
   return (
     <div className="flex justify-between items-center mx-4 sm:mx-10 gap-4 sm:gap-10 pt-4">
       <div className="rounded-md">
-        <Link to="/"><img src={logo} alt="logo" className="w-24 sm:w-20" /></Link>
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-24 sm:w-20" />
+        </Link>
         <span className="font-semibold absolute left-1/2 transform -translate-x-1/2 translate-y-10 invisible group-hover:visible bg-white text-black text-sm tracking-widest rounded-md py-1 px-2 whitespace-nowrap">
           Logo
         </span>
@@ -64,9 +99,20 @@ const Header = ({ mute, handleMuteButton, how_to_play,start_tour }: Props) => {
         <a href="#">
           <FaPlusCircle />
           <span className="font-semibold absolute left-1/2 transform -translate-x-1/2 translate-y-10 invisible group-hover:visible bg-white text-black text-sm tracking-widest rounded-md py-1 px-2 whitespace-nowrap">
-            Start Tic Tac Toe Tour
+            Start Tour
           </span>
         </a>
+      </div>
+      <div
+        id="share"
+        className="relative group text-2xl sm:text-3xl cursor-pointer m-1"
+      >
+        <div onClick={handleShare}>
+          <FaShareNodes />
+          <span className="font-semibold absolute left-1/2 transform -translate-x-1/2 translate-y-10 invisible group-hover:visible bg-white text-black text-sm tracking-widest rounded-md py-1 px-2 whitespace-nowrap">
+            Let the world know about this...
+          </span>
+        </div>
       </div>
       <div
         id="quine-vote"
@@ -100,6 +146,10 @@ const Header = ({ mute, handleMuteButton, how_to_play,start_tour }: Props) => {
           {mute ? "Unmute" : "Mute"}
         </span>
       </div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        {modalMessage}
+      </Modal>
     </div>
   );
 };
---
+++ src/components/Modal.tsx
@@ -0,0 +1,29 @@
+interface Props {
+  show: boolean;
+  onClose: () => void;
+  children: string;
+}
+
+const Modal = ({ show, onClose, children }: Props) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white p-3 rounded-lg shadow-lg text-center text-black flex flex-col justify-center items-center">
+        <div className="w-2/3 text-center text-lg font-semibold my-5 tracking-wider">
+          {children}
+        </div>
+        <button
+          onClick={onClose}
+          className="mt-4 px-4 py-2 w-full  bg-black text-white rounded-lg"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
